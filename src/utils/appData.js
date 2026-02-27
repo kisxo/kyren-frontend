440216fccd78eda952f6723cb55db965ba5778d5
uday <udaysubba2004@gmail.com>
2026-02-27 13:08:41 +0530

    update API and App URLs to point to yodhhabuilders.cloud

--- src/utils/appData.js
+++ src/utils/appData.js
@@ -3,8 +3,8 @@
 // export const AppUrl = "https://wurustore.in";
 // export const ApiUrl = "https://kyren.magicminute.online/api";
 // export const AppUrl = "https://kyren.magicminute.online";
-export const ApiUrl = "https://wurustore.in/api";
-export const AppUrl = "https://wurustore.in";
+export const ApiUrl = "https://yodhhabuilders.cloud/api";
+export const AppUrl = "https://yodhhabuilders.cloud";
 
 //dev api url
-//export const ApiUrl = "http://localhost:3000/api/";
+//export const ApiUrl = "http://localhost:3000/api/";LB8rUHrCRT5hTRmK
